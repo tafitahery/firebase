89de2d07c43fafcb4f31eb5f4d016fd7aaf0e9f5
Tafita Hery <tafitahery@gmail.com>
2023-03-10 17:32:20 +0300

    React Sidebar Design (Menu Bar)

---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,88 @@
+import './sidebar.scss';
+
+import {
+  AccountCircleOutlined,
+  CreditCard,
+  Dashboard,
+  ExitToApp,
+  InsertChart,
+  LocalShipping,
+  NotificationsNone,
+  PersonOutlineOutlined,
+  PsychologyAltOutlined,
+  SettingsApplications,
+  SettingsSystemDaydreamOutlined,
+  Store,
+} from '@mui/icons-material';
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">leboadmin</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <Dashboard className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LISTS</p>
+          <li>
+            <PersonOutlineOutlined className="icon" />
+            <span>Users</span>
+          </li>
+          <li>
+            <Store className="icon" />
+            <span>Products</span>
+          </li>
+          <li>
+            <CreditCard />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShipping className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <InsertChart className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNone className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SettingsSystemDaydreamOutlined className="icon" />
+            <span>Systel Health</span>
+          </li>
+          <li>
+            <PsychologyAltOutlined className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsApplications className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleOutlined className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToApp className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,5 +1,11 @@
+import Sidebar from '../../components/sidebar/Sidebar';
 import './home.scss';
 
 export default function Home() {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">container</div>
+    </div>
+  );
 }
