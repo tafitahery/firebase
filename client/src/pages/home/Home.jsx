7f5a4c28c4eb55ba665966c9f9ff377da4fdd4b6
Tafita Hery <tafitahery@gmail.com>
2023-03-11 14:13:35 +0300

    React Reusable Widget Components

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import './home.scss';
 
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
+import Widget from '../../components/widget/Widget';
 
 export default function Home() {
   return (
@@ -9,7 +10,12 @@ export default function Home() {
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        home container
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
       </div>
     </div>
   );
