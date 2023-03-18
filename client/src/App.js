95aed97694f18acc9860c6da1ffe407daa357683
Tafita Hery <tafitahery@gmail.com>
2023-03-18 17:43:40 +0300

    React Form Page Design

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import List from './pages/list/List';
 import New from './pages/new/New';
 import Single from './pages/single/Single';
 
+import { productInputs, userInputs } from './formSource';
+
 function App() {
   return (
     <div className="App">
@@ -17,12 +19,18 @@ function App() {
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add New User" />}
+              />
             </Route>
             <Route path="products">
               <Route index element={<List />} />
               <Route path=":productId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add New Product" />}
+              />
             </Route>
           </Route>
         </Routes>
--- client/src/pages/new/New.jsx
+++ client/src/pages/new/New.jsx
@@ -1,5 +1,63 @@
 import './new.scss';
 
-export default function New() {
-  return <div>New</div>;
+import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
+
+import { DriveFolderUploadOutlined } from '@mui/icons-material';
+import { useState } from 'react';
+
+export default function New({ inputs, title }) {
+  const [file, setFile] = useState('');
+
+  console.log(file);
+
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1 className="title">{title}</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : 'https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'
+              }
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutlined className="icon" />
+                </label>
+                <input
+                  type="file"
+                  id="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  style={{ display: 'none' }}
+                />
+              </div>
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label htmlFor={input.id}>{input.label}</label>
+                  <input
+                    type={input.type}
+                    placeholder={input.placeholder}
+                    id={input.id}
+                  />
+                </div>
+              ))}
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
