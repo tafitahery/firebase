072445770001c2a71e550729474c919a045fb46e
Tafita Hery <tafitahery@gmail.com>
2023-03-26 17:51:08 +0300

    React Firebase Login

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,14 +1,46 @@
-import { useState } from 'react';
 import './login.scss';
+
+import { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+
+import { auth } from '../../firebase';
 
 export default function Login() {
   const [error, setError] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        navigate('/');
+      })
+      .catch((error) => {
+        setError(true);
+        // ..
+      });
+  };
 
   return (
     <div className="login">
-      <form>
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button>Login</button>
         {error && <span>Error email or password!</span>}
       </form>
