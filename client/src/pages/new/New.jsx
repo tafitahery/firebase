357210af39fbb56b1bc0905eccc36ecdf556ade7
Tafita Hery <tafitahery@gmail.com>
2023-03-29 11:03:34 +0300

    Firebase CRUD: How to create data?

--- client/src/pages/new/New.jsx
+++ client/src/pages/new/New.jsx
@@ -1,15 +1,43 @@
 import './new.scss';
 
+import { DriveFolderUploadOutlined } from '@mui/icons-material';
+import { useState } from 'react';
+import { doc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+
+import { auth, db } from '../../firebase';
+
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 
-import { DriveFolderUploadOutlined } from '@mui/icons-material';
-import { useState } from 'react';
-
 export default function New({ inputs, title }) {
   const [file, setFile] = useState('');
+  const [data, setData] = useState({});
 
-  console.log(file);
+  const handleInput = (e) => {
+    const id = e.target.id;
+    const value = e.target.value;
+
+    setData({ ...data, [id]: value });
+  };
+
+  const handleAdd = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+      await setDoc(doc(db, 'users', res.user.uid), {
+        ...data,
+        timeStamp: serverTimestamp(),
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="new">
@@ -31,7 +59,7 @@ export default function New({ inputs, title }) {
             />
           </div>
           <div className="right">
-            <form>
+            <form onSubmit={handleAdd}>
               <div className="formInput">
                 <label htmlFor="file">
                   Image: <DriveFolderUploadOutlined className="icon" />
@@ -50,6 +78,7 @@ export default function New({ inputs, title }) {
                     type={input.type}
                     placeholder={input.placeholder}
                     id={input.id}
+                    onChange={handleInput}
                   />
                 </div>
               ))}
